ab265ef08633a132019f3fb0380d28f49c524181
horsemaker <yash_ghodekar@yahoo.in>
2021-06-24 16:10:55 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 var btnTranslate = document.querySelector('#btn-translate')
 var txtInput = document.querySelector('#txt-input')
-
-// live challenge queries
-var textArea = document.querySelector('textarea')
-// console.log(textArea)
-var btnPrimary = document.querySelector('.btn-primary')
-// console.log(btnPrimary)
-var inputBtn = document.querySelector('#input-btn')
-// console.log(inputBtn)
-var nameTranslator = document.querySelector('[name=translator]')
-// console.log(nameTranslator)
-
-// console.log(txtInput)
+var outputDiv = document.querySelector('#output')
 
 btnTranslate.addEventListener('click', clickEventHandler)
 
 function clickEventHandler() {
-  console.log('clicked!')
-  console.log('input: ', txtInput.value)
+  outputDiv.innerText = 'ehehehehehehehe ' + txtInput.value
 }
