7f2324a54201dc43f60608c6539a0638d7cc941d
horsemaker <yash_ghodekar@yahoo.in>
2021-06-24 15:22:52 +0530

    live challenge done

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var btnTranslate = document.querySelector('#btn-translate')
 var txtInput = document.querySelector('#txt-input')
 
+// live challenge queries
+var textArea = document.querySelector('textarea')
+// console.log(textArea)
+var btnPrimary = document.querySelector('.btn-primary')
+// console.log(btnPrimary)
+var inputBtn = document.querySelector('#input-btn')
+// console.log(inputBtn)
+var nameTranslator = document.querySelector('[name=translator]')
+// console.log(nameTranslator)
+
 // console.log(txtInput)
 
 btnTranslate.addEventListener('click', clickEventHandler)
