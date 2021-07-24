7d8eecc2886ab51076b4af50324404d7510413fd
horsemaker <yash_ghodekar@yahoo.in>
2021-07-24 11:41:28 +0530

    fix: outputDiv

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
 var btnTranslate = document.querySelector('#btn-translate')
 var txtInput = document.querySelector('#txt-input')
-var outputDiv = document.querySelector('#output')
+var outputDiv = document.querySelector('#translated-output')
 
 // var serverURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
 
 var serverURL = 'https://api.funtranslations.com/translate/minion.json'
 
 function getTranslationURL(text) {
-  return serverURL + '?' + 'text=' + text
+  return serverURL + '?text=' + text
 }
 
 function errorHandler(error) {
   console.log('error occured', error)
-  alert('something wrong with server! try again after some time')
+  alert('Something wrong with server! try again after some time')
 }
 
 function clickEventHandler() {
   // outputDiv.innerText = 'ehehehehehehehe ' + txtInput.value
-  var inputText = txtInput.value //taking input
+  console.log(btnTranslate)
+  console.log(txtInput)
+  console.log(outputDiv)
 
-  fetch(getTranslationURL(inputText))
-    .then((response) => response.json())
+  var inputText = txtInput.value //taking input
+  var translationURL = getTranslationURL(inputText)
+  console.log(translationURL)
+
+  fetch(translationURL)
+    .then((response) => {
+      console.log(response)
+      response.json()
+    })
     .then((json) => {
+      console.log(json)
       var translatedText = json.contents.translated
-      outputDiv.innerText = translatedText // output
+      outputDiv.textContent = translatedText // output
     })
     .catch(errorHandler)
 }
